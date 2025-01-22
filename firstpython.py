4ae472df856efca807597e9440961581a0e14ea1
SASHI  SEKHAR  R <officialsashisekhar1@gmail.com>
2025-01-22 23:20:47 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,1 @@
-#Display the output
-
-print("New Python File")
+https://cf-courses-data.static.labs.skills.network/jupyterlite/2.5.5/lab/index.html?path=PY0101EN%2Flabs%2FModule+1%2Ffirst_JupyterNB.ipynb
